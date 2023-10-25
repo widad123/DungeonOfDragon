33784a9ea13dfc3310b3ee5d1d54a116d23efad3
Asmae Tnani <93820154+Asmaetnani@users.noreply.github.com>
2023-10-25 16:55:56 +0200

    Create Character.java

---
+++ src/Character.java
@@ -0,0 +1,37 @@
+public class Character {
+
+    //la force de l'arme
+    protected int m_iLifePoints;
+    protected Weapon m_Weapon;
+
+    public Character(int LifePoints){
+        this.m_iLifePoints= LifePoints;
+    }
+
+    public void attack(Character p_TargetCharacter){
+        if (m_Weapon != null) {
+            m_Weapon.inflictDamage(targetCharacter);
+        }
+    }
+
+    public int getNbLifePoints(){
+        return m_iLifePoints;
+    }
+
+    //p_iDamages c'est le montant de dégâts que le personnage doit subir.
+    public void receiveDamages(int p_iDamages){
+
+        m_iLifePoints -= p_iDamages;
+
+    }
+
+    public boolean isAlive(){
+
+        if (m_iLifePoints>0) return true;
+        else return false;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.m_Weapon = weapon;
+    }
+}
