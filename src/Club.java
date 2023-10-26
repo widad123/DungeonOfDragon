f8018be357adcc221d7bbb70c779165042dbd953
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:03:50 +0200

    Update Club.java

--- src/Club.java
+++ src/Club.java
@@ -1,5 +1,13 @@
 public class Club extends MonsterWeapon{
+    /**
+     * The `Club` class represents a specific monster weapon: a club.
+     * This class inherits from the `MonsterWeapon` class.
+     */
     public Club() {
+        /**
+         * Default constructor for the `Club` class.
+         * Initializes the club's attack points (10), critical countdown (12), and maximum rounds before critical (9).
+         */
         super(10,12,9);
 
     }
