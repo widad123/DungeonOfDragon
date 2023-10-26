8d062ccab99911dff1fe22f15e1ad27626711567
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 15:57:23 +0200

    Update HeroWeapon.java

--- src/HeroWeapon.java
+++ src/HeroWeapon.java
@@ -1,29 +1,44 @@
+/**
+ * The `HeroWeapon` class represents a type of weapon used by heroes.
+ * This class extends the `Weapon` class and adds attack bonus capabilities.
+ */
+
 public class HeroWeapon extends Weapon{
-    private int m_iAttaqueBonus;
-    private String p_TargetCharacter;
+    protected int m_iAttaqueBonus;
 
-    private int p_iBonus;
+    protected int p_iBonus;
 
+    /**
+     * Constructs a new `HeroWeapon` with the specified attack points and attack bonus.
+     *
+     * @param m_AttackPoints The base attack points of the weapon.
+     * @param m_AttackBonus The bonus attack points provided by the weapon.
+     */
 
-    public HeroWeapon(int m_AttackPoints,int m_AttackBonus, int p_iBonus) {
+    public HeroWeapon(int m_AttackPoints,int m_AttackBonus) {
         super(m_AttackPoints);
         this.m_iAttaqueBonus= m_iAttaqueBonus;
     }
+
+    /**
+     * Get the attack bonus value provided by the weapon.
+     *
+     * @return The attack bonus value.
+     */
+
 
     public int getM_iAttaqueBonus() {
         return m_iAttaqueBonus;
     }
 
-    public String getP_TargetCharacter() {
-        return p_TargetCharacter;
-    }
+    /**
+     * Set the attack bonus value provided by the weapon.
+     *
+     * @param m_AttackBonus  The new attack bonus value to set.
+     */
 
-    public int getP_iBonus() {
-        return p_iBonus;
-    }
-
-    public void setAttackBonus(int p_iBonus) {
-        this.p_iBonus = p_iBonus;
+    public void setAttackBonus(int m_AttackBonus ) {
+        this.p_iBonus = m_AttackBonus ;
     }
 
 
