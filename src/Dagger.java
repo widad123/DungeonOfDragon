bc98c04fcb31c086453ed6a44ac28bb188626e07
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:01:07 +0200

    Update Dagger.java

--- src/Dagger.java
+++ src/Dagger.java
@@ -1,6 +1,13 @@
 public class Dagger extends MonsterWeapon{
-
+    /**
+     * The `Dagger` class represents a specific monster weapon: a dagger.
+     * This class inherits from the `MonsterWeapon` class.
+     */
     public Dagger() {
+        /**
+         * Default constructor for the `Dagger` class.
+         * Initializes the dagger's attack points (2), critical countdown (4), and maximum rounds before critical (5).
+         */
         super(2,4,5);
 
     }
