52302f88acefb21ca89de4713c1b9bf47df75556
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:01:51 +0200

    Update Bite.java

--- src/Bite.java
+++ src/Bite.java
@@ -1,5 +1,13 @@
 public class Bite extends MonsterWeapon{
-    public Dagger() {
+    /**
+     * The `Bite` class represents a specific monster weapon: a bite.
+     * This class inherits from the `MonsterWeapon` class.
+     */
+    public Bite() {
+        /**
+         * Default constructor for the `Bite` class.
+         * Initializes the bite's attack points (4), critical countdown (6), and maximum rounds before critical (6).
+         */
         super(4,6,6);
 
     }
