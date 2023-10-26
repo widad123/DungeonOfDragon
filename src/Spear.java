a18bf26ac7a017939e892676da9a89253f7eb65a
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:00:04 +0200

    Update Spear.java

--- src/Spear.java
+++ src/Spear.java
@@ -1,6 +1,14 @@
-public class Sword extends HeroWeapon {
-    public Sword() {
-        super(15,9);
+public class Spear extends HeroWeapon{
+    /**
+     * The `Spear` class represents a specific hero weapon: a spear.
+     * This class inherits from the `HeroWeapon` class.
+     */
+    public Spear() {
+        /**
+         * Default constructor for the `Spear` class.
+         * Initializes the spear's attack points (20) and attack bonus (12).
+         */
+        super(20,12);
 
     }
 }
