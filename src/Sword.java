2a47f38faba1098d1de8832c466100ca1f1f7ac7
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 15:59:37 +0200

    Update Sword.java

--- src/Sword.java
+++ src/Sword.java
@@ -1,2 +1,14 @@
 public class Sword extends HeroWeapon {
+    /**
+     * The `Sword` class represents a specific hero weapon: a sword.
+     * This class inherits from the `HeroWeapon` class.
+     */
+    public Sword() {
+        /**
+         * Default constructor for the `Sword` class.
+         * Initializes the sword's attack points (15) and attack bonus (9).
+         */
+        super(15,9);
+
+    }
 }
