7e88264a76947d29e6eeece1b0d042ec3db3cc29
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 15:58:46 +0200

    Update WaterFlask.java

--- src/WaterFlask.java
+++ src/WaterFlask.java
@@ -1,5 +1,13 @@
 public class WaterFlask extends HeroWeapon {
+    /**
+     * The `WaterFlask` class represents a specific hero weapon: a water flask.
+     * This class inherits from the `HeroWeapon` class.
+     */
     public WaterFlask() {
+        /**
+         * Default constructor for the `WaterFlask` class.
+         * Initializes the water flask's attack points (10) and attack bonus (6).
+         */
         super(10,6);
 
     }
