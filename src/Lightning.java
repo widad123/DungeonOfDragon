56ba042dc3f9c1621d91dd4426a72c0bc39dd080
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:03:21 +0200

    Update Lightning.java

--- src/Lightning.java
+++ src/Lightning.java
@@ -1,5 +1,13 @@
 public class Lightning extends MonsterWeapon{
+    /**
+     * The `Lightning` class represents a specific monster weapon: lightning.
+     * This class inherits from the `MonsterWeapon` class.
+     */
     public Lightning() {
+        /**
+         * Default constructor for the `Lightning` class.
+         * Initializes the lightning's attack points (8), critical countdown (10), and maximum rounds before critical (8).
+         */
         super(8,10,8);
 
     }
