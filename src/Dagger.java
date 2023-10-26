9725e25a312413c3d3913a3826d4e4a4c74b2e3b
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 12:16:05 +0200

    Update Dagger.java

--- src/Dagger.java
+++ src/Dagger.java
@@ -1,6 +1,8 @@
 public class Dagger extends MonsterWeapon{
 
-    public Dagger(int m_AttackPoints, int m_CriticalCountDown, int s_iMaxRoundBeforeCritical) {
-        super(m_AttackPoints, m_CriticalCountDown, s_iMaxRoundBeforeCritical);
+    public Dagger() {
+        super(2,4,5);
+
     }
 }
+
