d18eb4a6bff71fad935a06151d6a4c9e8f08559a
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 12:14:44 +0200

    Update Bite.java

--- src/Bite.java
+++ src/Bite.java
@@ -1,5 +1,7 @@
 public class Bite extends MonsterWeapon{
-    public Bite(int m_AttackPoints, int m_CriticalCountDown, int s_iMaxRoundBeforeCritical) {
-        super(m_AttackPoints, m_CriticalCountDown, s_iMaxRoundBeforeCritical);
+    public Dagger() {
+        super(4,6,6);
+
     }
+
 }
