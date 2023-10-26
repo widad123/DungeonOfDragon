85517b6b66880d62019f268bcbe2691166d47510
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:00:29 +0200

    Update FireArrow.java

--- src/FireArrow.java
+++ src/FireArrow.java
@@ -1,5 +1,13 @@
 public class FireArrow extends HeroWeapon{
+    /**
+     * The `FireArrow` class represents a specific hero weapon: a fire arrow.
+     * This class inherits from the `HeroWeapon` class.
+     */
     public FireArrow() {
+        /**
+         * Default constructor for the `FireArrow` class.
+         * Initializes the fire arrow's attack points (25) and attack bonus (15).
+         */
         super(25,15);
 
     }
