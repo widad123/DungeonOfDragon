aba57aead90239314194eef8cf1cf8736f60d6d6
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 15:58:20 +0200

    Update Arrow.java

--- src/Arrow.java
+++ src/Arrow.java
@@ -1,5 +1,13 @@
 public class Arrow extends HeroWeapon {
+    /**
+     * The `Arrow` class represents a specific hero weapon: an arrow.
+     * This class inherits from the `HeroWeapon` class.
+     */
     public Arrow() {
+        /**
+         * Default constructor for the `Arrow` class.
+         * Initializes the arrow's attack points (5) and attack bonus (3).
+         */
         super(5,3);
 
     }
