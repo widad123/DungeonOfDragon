8b8c11adb483c4784ee1fa515397bf5955c8d656
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 16:02:34 +0200

    Update Axe.java

--- src/Axe.java
+++ src/Axe.java
@@ -1,6 +1,17 @@
-public class Club extends MonsterWeapon{
-    public Club() {
-        super(10,12,9);
+import java.util.Random;
+
+public class Axe extends MonsterWeapon {
+    /**
+     * The `Axe` class represents a specific monster weapon: an axe.
+     * This class inherits from the `MonsterWeapon` class.
+     */
+    public Axe() {
+        /**
+         * Default constructor for the `Axe` class.
+         * Initializes the axe's attack points (6), critical countdown (8), and maximum rounds before critical (7).
+         */
+        super(6,8,7);
 
     }
 }
+
