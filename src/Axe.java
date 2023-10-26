1bf9920369fddcf88c4860b9ee1a8e7dfbd52b11
Amelderbali <123973853+Amelderbali@users.noreply.github.com>
2023-10-26 12:13:50 +0200

    Update Axe.java

--- src/Axe.java
+++ src/Axe.java
@@ -1,5 +1,6 @@
-public class Axe extends MonsterWeapon{
-    public Axe(int m_AttackPoints, int m_CriticalCountDown, int s_iMaxRoundBeforeCritical) {
-        super(m_AttackPoints, m_CriticalCountDown, s_iMaxRoundBeforeCritical);
+public class Club extends MonsterWeapon{
+    public Club() {
+        super(10,12,9);
+
     }
 }
