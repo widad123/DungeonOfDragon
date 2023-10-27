27e54d8aeff3d1e2522b43f78b9cc1f1340d07d1
zakiaFaraji <93118597+zakiaFaraji@users.noreply.github.com>
2023-10-27 11:11:55 +0200

    Update Run.java

--- src/Run.java
+++ src/Run.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
+/**
+ * The Run class contains the main method to run the game.
+ */
+
 public class Run {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+      /**
+         * Read the number of rooms from the user input.
+        */
         int nbRoom = scan.nextInt();
         int i =0;
         do{
+      /**
+        *Create a new dungeon with 6 rooms.
+       */
+            
             Dungeon d1 = new Dungeon(6);
             i++;
         }while( nbRoom >= i);
