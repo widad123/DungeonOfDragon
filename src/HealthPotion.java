d776f6f2e5b076d54a731ae468bd6af6566a9338
zakiaFaraji <93118597+zakiaFaraji@users.noreply.github.com>
2023-10-27 10:47:04 +0200

    Update HealthPotion.java

--- src/HealthPotion.java
+++ src/HealthPotion.java
@@ -1,14 +1,36 @@
+/**
+ * The HealthPotion class represents a health potion item in the game, which can be used by heroes.
+ */
+
 public class HealthPotion extends Item{
+
+        /**
+     * Constructs a health potion with the specified effect value.
+     *
+     * @param effectValue The amount by which the health of the hero is improved when using the potion.
+     */
 
     public HealthPotion(int effectValue) {
         super(effectValue);
     }
 
+        /**
+     * Applies the health-improving effect of the potion to a hero.
+     *
+     * @param p_hero The hero to whom the health potion's effect is applied.
+     */
+    
     @Override
     public void applyEffect(Hero p_hero) {
         p_hero.improveHealth(m_iEffectValue);
     }
 
+    /**
+     * Gets the effect value of the health potion, which represents the amount of health improvement it provides.
+     *
+     * @return The effect value of the health potion.
+     */
+    
     public int getEffectValue() {
         return m_iEffectValue;
     }
