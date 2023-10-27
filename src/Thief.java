4343b204f9c4658044b2c6cf69920d2613ae503d
zakiaFaraji <93118597+zakiaFaraji@users.noreply.github.com>
2023-10-27 11:35:50 +0200

    Update Thief.java

--- src/Thief.java
+++ src/Thief.java
@@ -1,5 +1,12 @@
+/**
+ * The Thief class represents a type of monster known as a "Thief" in the game.
+ */
 public class Thief extends Monster {
 
+        /**
+     * Constructs a Thief monster.
+     */
+    
     public Thief() {
         super();
         s_iNbThiefInstances++;
