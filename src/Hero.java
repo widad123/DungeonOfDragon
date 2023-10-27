2c6df6ae4f278333a5dca2ecb9f60d09935314f1
zakiaFaraji <93118597+zakiaFaraji@users.noreply.github.com>
2023-10-27 10:54:24 +0200

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,12 +1,36 @@
 import java.util.Scanner;
+
+    /**
+ * The Hero class represents a character in the game, the main protagonist.
+ */
+
 public class Hero  {
-
+    
+    /** The strength attribute of the hero. */
     private int m_iStrength;
-    private int lifePoints;
-    private int strengthPoints;
-    private boolean[] searchedRooms;
-    private boolean[] searchedMonsters;
 
+    /** The current life points of the hero. */
+    private int lifePoints;
+
+    /** The strength points of the hero. */
+    private int strengthPoints;
+
+     /** An array to keep track of discovered rooms. */
+    private boolean[] searchedRooms;
+
+     /** An array to keep track of discovered rooms. */
+    private boolean[] searchedMonsters;
+    
+        /**
+     * Constructs a hero with the specified attributes.
+     *
+     * @param m_iStrength The strength attribute of the hero.
+     * @param lifePoints The initial life points of the hero.
+     * @param strengthPoints The initial strength points of the hero.
+     * @param numRooms The number of rooms in the game.
+     * @param numMonsters The number of different types of monsters in the game.
+     */
+    
     public Hero(int m_iStrength, int lifePoints, int strengthPoints,int numRooms,int numMonsters) {
         this.m_iStrength = m_iStrength;
         this.lifePoints = lifePoints;
@@ -15,6 +39,12 @@ public class Hero  {
         this.searchedMonsters = new boolean[numMonsters + 1];
     }
 
+        /**
+     * Allows the hero to select a weapon from a predefined list.
+     *
+     * @param p_sWeaponClassName The name of the weapon to be selected.
+     */
+    
     public void selectWeaponFromArslan(String p_sWeaponClassName) {
         String[] Arslan = {"Arrow", "WaterFlask", "Sword", "Spear", "FireArrow"};
         Scanner scanner = new Scanner(System.in);
@@ -37,6 +67,13 @@ public class Hero  {
             System.out.println("L'arme " + userInput + " n'a pas été trouvée dans le tableau Arslan.");
         }
     }
+
+    /**
+     * Simulates discovering and encountering different types of monsters in rooms.
+     *
+     * @param p_Monster The monster to be encountered.
+     */
+    
     public void discoverEnemy(Monster p_Monster) {
         String[] monsterNames = {"Troll", "Thief","Sorcerer","Zombie","Barbarian"};
 
@@ -53,18 +90,44 @@ public class Hero  {
         }
     }
 
+            /**
+     * Sets the strength attribute of the hero.
+     *
+     * @param p_iStrength The new strength value for the hero.
+     */
+
+    
     public void setStrength(int p_iStrength) {
         this.m_iStrength = p_iStrength;
     }
-
+    
+        /**
+     * Sets the strength attribute of the hero.
+     *
+     * @param p_iStrength The new strength value for the hero.
+     */
+    
     public void improveHealth(int p_iValue) {
         this.lifePoints += p_iValue;
     }
 
+   /**
+     * Improves the strength attribute of the hero by a specified value.
+     *
+     * @param p_iValue The amount by which the strength attribute is improved.
+     */
+    
     public void improveStrength(int p_iValue) {
         this.strengthPoints = p_iValue;
     }
 
+        /**
+     * Searches for potions in a room and on a defeated monster, and improves the hero's attributes accordingly.
+     *
+     * @param roomNumber The room number in which the hero is searching for potions.
+     * @param monsterNumber The number of the defeated monster on which the hero is searching for potions.
+     */
+    
     public void searchForPotion(int roomNumber, int monsterNumber) {
         if (searchedRooms[roomNumber]) {
             System.out.println("Sorry, you have already found everything in this room");
