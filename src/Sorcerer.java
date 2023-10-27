7d08f0df87bc20aa48964a2cd0047941addc3adf
zakiaFaraji <93118597+zakiaFaraji@users.noreply.github.com>
2023-10-27 14:23:20 +0200

    Update Sorcerer.java

--- src/Sorcerer.java
+++ src/Sorcerer.java
@@ -1,19 +1,8 @@
-/**
- * The Sorcerer class represents a type of monster known as a "Sorcerer" in the game.
- */
 public class Sorcerer extends Monster {
-
-        /**
-     * Constructs a Sorcerer monster.
-     */
-    
     public Sorcerer() {
-        super();
-        s_iNbSorcerInstances++;
-        this.label = "Sorcerer";
-        m_sEffectiveWeaponType = "WaterFlask";
-        System.out.format("Le type de monstre est %s le nombre de %s est : %d l'arme qui lui fait des dégats à ce monstre est %s %n",this.label,this.label,s_iNbTrollInstances, m_sEffectiveWeaponType);
-
-
+        super("Sorcerer", 35, 120, new Lightning());
     }
+
+    // Personnalisez les attributs du Sorcerer en fonction des règles du jeu, par exemple, sa force, ses points de vie, et son arme.
 }
+
