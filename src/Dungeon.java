fdd593c3922c2516d1426db4ce822828bebfee49
zakiaFaraji <93118597+zakiaFaraji@users.noreply.github.com>
2023-10-27 10:42:04 +0200

    Update Dungeon.java

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,7 +1,19 @@
+/**
+ * The Dungeon class represents a dungeon in a game, consisting of multiple rooms.
+ */
+
 public class Dungeon {
 
+        /** An array of rooms in the dungeon. */
+    
     private Room [] roomArray;
 
+        /**
+     * Constructs a dungeon with the specified number of rooms.
+     *
+     * @param nbRoom The number of rooms in the dungeon.
+     */
+    
     public Dungeon(int nbRoom) {
         this.roomArray = new Room[nbRoom];
         for (int i = 1; i < roomArray.length; i++){
@@ -10,13 +22,17 @@ public class Dungeon {
         }
     }
 
+        /**
+     * Retrieves a specific room within the dungeon based on its index.
+     *
+     * @param p_iRoomIndex The index of the room to retrieve.
+     * @return The room at the specified index.
+     */
+    
     public Room getRoom(int p_iRoomIndex){
         return this.roomArray[p_iRoomIndex];
     }
 
-    //public void greetHeros(Hero p_Hero){
-
-    //}
 
 
 }
